099341a61d0293c8b4762b9a0869f90b624603d4
diegonmarcos <diegonmarcos@gmail.com>
2024-06-15 02:25:42 +0000

    a

---
+++ Common/Circle 1_Libft/Bonus_Lists/ft_lstmap.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_elem;
+	t_list	*new_list;
+
+	if (!lst || !f)
+		return (NULL);
+	new_list = NULL;
+	while (lst)
+	{
+		new_elem = ft_lstnew(f(lst->content));
+		if (!new_elem)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_elem);
+		lst = lst->next;
+	}
+	return (new_list);
+}
--- Common/Circle 1_Libft/libft.h
+++ Common/Circle 1_Libft/libft.h
@@ -38,7 +38,7 @@ typedef struct s_list
     struct s_list *next;
 }   t_list;
 
-// ...(Bonus function prototypes) ...
+// ...Bonus function prototypes ...
 t_list *ft_lstnew(void *content);
 void    ft_lstadd_front(t_list **lst, t_list *new);
 int     ft_lstsize(t_list *lst);
