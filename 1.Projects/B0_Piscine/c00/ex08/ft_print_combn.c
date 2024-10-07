07d83d19708714931b77d1b17d8863cb28951f61
Diego <diegonmarcos@gmail.com>
2024-10-07 16:23:25 +0200

    rush00

--- 1.Projects/B0_Piscine/c00/ex08/ft_print_combn.c
+++ 1.Projects/B0_Piscine/c00/ex08/ft_print_combn.c
@@ -6,7 +6,32 @@
 /*   By: dinepomu <dinepomu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:12:48 by dinepomu          #+#    #+#             */
-/*   Updated: 2024/10/07 16:12:54 by dinepomu         ###   ########.fr       */
+/*   Updated: 2024/10/07 16:23:24 by dinepomu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void print_coll(int n)
+
+
+void ft_print_combn(int n)
+{
+    int i;
+
+    i = 0;
+    while(i < n)
+    {
+        write(1, i, 1)
+        while(j < n)
+        {
+            write(1, j, 1)
+        }
+    }
+}
+
+/*
+int main (void)
+{
+    ft_print_combn(2);
+    return 0;
+}
+*/
