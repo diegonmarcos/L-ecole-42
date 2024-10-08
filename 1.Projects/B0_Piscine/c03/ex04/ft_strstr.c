69e723903fa51a674b012761f162c97530528062
Diego <diegonmarcos@gmail.com>
2024-10-08 14:35:03 +0200

    ef

--- 1.Projects/B0_Piscine/c03/ex04/ft_strstr.c
+++ 1.Projects/B0_Piscine/c03/ex04/ft_strstr.c
@@ -1,17 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   a.c                                                :+:      :+:    :+:   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dnepomuc <dnepomuc@student.42barcel>       +#+  +:+       +#+        */
+/*   By: dinepomu <dinepomu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 18:58:41 by dnepomuc          #+#    #+#             */
-/*   Updated: 2024/03/24 19:02:08 by dnepomuc         ###   ########.fr       */
+/*   Updated: 2024/10/08 14:34:55 by dinepomu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
-#include <stdio.h>
 
 char	*ft_strstr(char *str, char *to_find)
 {
@@ -33,11 +30,30 @@ char	*ft_strstr(char *str, char *to_find)
 	}
 	return (0);
 }
-
-/*int	main()
+/*
+#include <stdio.h>
+#include <string.h>
+int	main()
 {
-	char str[] = "42 1337 Network 2021 piscine Benguerir Khouribga";
-	char find[] = "piscine";
-	printf("%s", ft_strstr(str, find));
+	char str1[] = "Duck 42 Piscine Duck piscine 2021 piscine Born to Code";
+	char find1[] = "piscine";
+	printf("%s\n", ft_strstr(str1, find1));
+	printf("%s\n", strstr(str1, find1));
+	char str2[] = "";
+	char find2[] = "piscine";
+	printf("%s\n", ft_strstr(str2, find2));
+	printf("%s\n", strstr(str2, find2));
+	char str3[] = "Abc ab bc";
+	char find3[] = "";
+	printf("%s\n", ft_strstr(str3, find3));
+	printf("%s\n", strstr(str3, find3));
+	char str4[] = "";
+	char find4[] = "";
+	printf("%s\n", ft_strstr(str4, find4));
+	printf("%s\n", strstr(str4, find4));
+	char str5[] = "Duck 42 Piscine Duck 1p1iscine 2021 piscineBorntoCode 42 24 21 12";
+	char find5[] = "piscine";
+	printf("%s\n", ft_strstr(str5, find5));
+	printf("%s\n", strstr(str5, find5));
 	return 0;
 }*/
