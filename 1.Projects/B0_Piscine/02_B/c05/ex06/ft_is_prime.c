8a38b86772b67a7cc90206996bb8e2acb41f396e
diegonmarcos <diegonmarcos@gmail.com>
2024-10-16 17:40:09 +0200

    upd

--- 1.Projects/B0_Piscine/02_B/c05/ex06/ft_is_prime.c
+++ 1.Projects/B0_Piscine/02_B/c05/ex06/ft_is_prime.c
@@ -6,7 +6,30 @@
 /*   By: dinepomu <dinepomu@42.de>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 17:29:23 by dinepomu          #+#    #+#             */
-/*   Updated: 2024/10/16 17:29:35 by dinepomu         ###   ########.fr       */
+/*   Updated: 2024/10/16 17:39:57 by dinepomu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+•Create a function that returns 1 if the number given as a parameter is a prime
+number, and 0 if it isn’t.
+•Here’s how it should be prototyped :
+int ft_is_prime(int nb);
+0 and 1 are not prime numbers.
+*/
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	if (nb < 2)
+		return (0);
+	i = 2;
+	while (i * i <= nb)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
--- 1.Projects/B0_Piscine/02_B/c05/ex07/ft_find_next_prime.c
+++ 1.Projects/B0_Piscine/02_B/c05/ex07/ft_find_next_prime.c
@@ -6,7 +6,37 @@
 /*   By: dinepomu <dinepomu@42.de>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 17:29:27 by dinepomu          #+#    #+#             */
-/*   Updated: 2024/10/16 17:29:35 by dinepomu         ###   ########.fr       */
+/*   Updated: 2024/10/16 17:37:23 by dinepomu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+•Create a function that returns the next prime number greater or
+ equal to the number
+given as argument.
+•Here’s how it should be prototyped :
+int ft_find_next_prime(int nb);
+*/
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	if (nb < 2)
+		return (0);
+	i = 2;
+	while (i * i <= nb)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	while (!ft_is_prime(nb))
+		nb++;
+	return (nb);
+}
