3cfd1a6a497bf584377cc6b0a1181e8c423c0769
diegonmarcos <diegonmarcos@gmail.com>
2024-10-16 15:09:03 +0200

    up

--- 1.Projects/B0_Piscine/02_A/c04/ex04/ft_putnbr_base.c
+++ 1.Projects/B0_Piscine/02_A/c04/ex04/ft_putnbr_base.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 
 void	ft_putchar(char c)
 {
@@ -21,17 +20,25 @@ void	ft_putchar(char c)
 int	check_base(char *base)
 {
 	int	i;
+	int	j;
 
 	i = 0;
-	while (base[i] != '\0')
+	while (base[i])
 	{
-		if (base[i] == '+' || base[i] == '-' || base[i] == base[i + 1])
-			return (0);
+		if (base[i] == '+' || base[i] == '-')
+			return (1);
+		j = i + 1;
+		while (base[j])
+		{
+			if (base[i] == base[j] || base[i] < 32 || base[i] > 126)
+				return (1);
+			j++;
+		}
 		i++;
 	}
 	if (i <= 1)
-		return (0);
-	return (1);
+		return (1);
+	return (0);
 }
 
 void	print_base_nb(int nbr, char *base, int size)
@@ -42,7 +49,7 @@ void	print_base_nb(int nbr, char *base, int size)
 	if (nbr < 0)
 	{
 		write(1, "-", 1);
-		n = nbr * (-1);
+		n = -nbr;
 	}
 	else
 		n = nbr;
@@ -57,21 +64,33 @@ void	ft_putnbr_base(int nbr, char *base)
 	int	s;
 
 	s = 0;
-	if (check_base(base) == 1)
+	if (check_base(base) == 0)
 	{
-		while (base[s] != '\0')
+		while (base[s])
 			s++;
 		print_base_nb(nbr, base, s);
 	}
 }
 
-/*int		main(void)
+#include <limits.h>
+
+int	main(void)
 {
-	ft_putnbr_base(-12, "01");
-	printf("\n");
-	ft_putnbr_base(40, "poneyvif");
-	printf("\n");
-	ft_putnbr_base(894867, "0123456789");
-	printf("\n");
+	write(1, "-42 = ", 7);
+	ft_putnbr_base(-42, "01");
+	write(1, "\n", 1);
+	ft_putnbr_base(424242, "0123456789");
+	write(1, "\n", 1);
+	ft_putnbr_base(INT_MIN, "0123456789");
+	write(1, "\n", 1);
+	ft_putnbr_base(INT_MIN, "0123456789abcdef");
+	write(1, "\n", 1);
 	ft_putnbr_base(53, "0123456789abcdef");
-}*/
+	write(1, "\n", 1);
+	ft_putnbr_base(42, "");
+	write(1, "\n", 1);
+	ft_putnbr_base(42, "\v0123456789abcdef");
+	write(1, "\n", 1);
+	ft_putnbr_base(42, "duck");
+	write(1, "\n", 1);
+}
