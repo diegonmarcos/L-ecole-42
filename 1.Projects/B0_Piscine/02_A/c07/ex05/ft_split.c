d3915a73ebe6576982d225effeece8d358eac20d
Diego <diegonmarcos@gmail.com>
2024-10-18 18:57:16 +0200

    up

---
+++ 1.Projects/B0_Piscine/02_A/c07/ex05/ft_split.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dinepomu <dinepomu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/18 18:51:39 by dinepomu          #+#    #+#             */
+/*   Updated: 2024/10/18 18:51:59 by dinepomu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+•Create a function that splits a string of character depending on another string of
+characters.
+•You’ll have to use each character from the string charset as a separator.
+•The function returns an array where each element of the array contains the address
+of a string wrapped between two separators. The last element of that array should
+equal to 0 to indicate the end of the array.
+•There cannot be any empty strings in your array. Get your own conclusions ac-
+cordingly.
+•The string given as argument won’t be modifiable.
+•Here’s how it should be prototyped :
+char **ft_split(char *str, char *charset);
+
+*/
+
+char **ft_split(char *str, char *charset)
+{
+	
+}
