8e15acd21653c0b776d3ff90af161a6b437ae22d
Diego <diegonmarcos@gmail.com>
2024-10-18 18:52:09 +0200

    update

---
+++ 1.Projects/B0_Piscine/02_A/c07/ex04/ft_convert_base.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dinepomu <dinepomu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/18 18:49:08 by dinepomu          #+#    #+#             */
+/*   Updated: 2024/10/18 18:52:09 by dinepomu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+•Create a function that returns the result of the conversion of the string nbr from a
+base base_from to a base base_to.
+•nbr, base_from, base_to may be not writable.
+•nbr will follow the same rules as ft_atoi_base (from an other module). Beware of
+’+’, ’-’ and whitespaces.
+•The number represented by nbr must fit inside an int.
+•If a base is wrong, NULL should be returned.
+•The returned number must be prefix only by a single and uniq ’-’ if necessary, no
+whitespaces, no ’+’.
+•Here’s how it should be prototyped :
+char *ft_convert_base(char *nbr, char *base_from, char *base_to);
+*/
+
+char *ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+	
+}
