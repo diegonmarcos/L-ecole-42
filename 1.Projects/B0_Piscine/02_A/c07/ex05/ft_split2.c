c3d7b2527f0e2d1a29fd59208b53b889bdf66715
diegonmarcos <diegonmarcos@gmail.com>
2024-10-19 18:01:16 +0200

    up

--- 1.Projects/B0_Piscine/02_A/c07/ex05/ft_split2.c
+++ 1.Projects/B0_Piscine/02_A/c07/ex05/ft_split2.c
@@ -1,20 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_split.c                                         :+:      :+:    :+:   */
+/*   ft_split2.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dinepomu <dinepomu@42.de>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 18:51:39 by dinepomu          #+#    #+#             */
-<<<<<<< HEAD
-<<<<<<< HEAD
-/*   Updated: 2024/10/19 17:28:34 by dinepomu         ###   ########.fr       */
-=======
-/*   Updated: 2024/10/19 17:35:56 by dinepomu         ###   ########.fr       */
->>>>>>> 40333d1afe53834912e2386a1249be0c3f9ab6dc
-=======
-/*   Updated: 2024/10/19 17:36:15 by dinepomu         ###   ########.fr       */
->>>>>>> f825603 (u)
+/*   Updated: 2024/10/19 18:00:54 by dinepomu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +14,8 @@
 •Create a function that splits a string of character depending on another string 
 of characters.
 •You’ll have to use each character from the string charset as a separator.
-•The function returns an array where each element of the array contains the address
+•The function returns an array where each element of the array contains 
+the address
 of a string wrapped between two separators. The last element of that array should
 equal to 0 to indicate the end of the array.
 •There cannot be any empty strings in your array. Get your own conclusions ac-
@@ -44,45 +37,11 @@ char **ft_split(char *str, char *charset);
 **	ended by a NULL pointer.
 */
 
-<<<<<<< HEAD
-static int	count_words(char *str, char c)
-=======
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
-static int	is_delimiter(char c, char *charset)
-{
-	while (*charset)
-	{
-		if (c == *charset)
-			return (1);
-		charset++;
-	}
-	return (0);
-}
-
-static int	count_words(char *str, char *charset)
-{
-	int	i;
-	int	trigger;
-
-	i = 0;
-	trigger = 0;
-	while (*str)
-	{
-		if (!is_delimiter(*str, charset) && trigger == 0)
-		{
-			trigger = 1;
-			i++;
-		}
-		else if (is_delimiter(*str, charset))
-			trigger = 0;
-		str++;
-	}
-	return (i);
-}
-
-static char	*word_dup(char *str, int start, int finish)
+char	*word_dup(char *str, int start, int finish)
 {
 	char	*word;
 	int		i;
@@ -95,6 +54,40 @@ static char	*word_dup(char *str, int start, int finish)
 	return (word);
 }
 
+int	is_delim(char c, char *charset);
+
+int	count_words(char *str, char *charset)
+{
+	int	i;
+	int	trigger;
+
+	i = 0;
+	trigger = 0;
+	while (*str)
+	{
+		if (!is_delim(*str, charset) && !trigger)
+		{
+			trigger = 1;
+			i++;
+		}
+		else if (is_delim(*str, charset))
+			trigger = 0;
+		str++;
+	}
+	return (i);
+}
+
+int	is_delim(char c, char *charset)
+{
+	while (*charset)
+	{
+		if (c == *charset)
+			return (1);
+		charset++;
+	}
+	return (0);
+}
+
 char	**ft_split(char *str, char *charset)
 {
 	size_t	i;
@@ -102,16 +95,17 @@ char	**ft_split(char *str, char *charset)
 	int		index;
 	char	**split;
 
-	if (!str || !(split = malloc((count_words(str, charset) + 1) * sizeof(char *))))
+	split = malloc((count_words(str, charset) + 1) * sizeof(char *));
+	if (!str || !split)
 		return (0);
 	i = 0;
 	j = 0;
 	index = -1;
 	while (i <= strlen(str))
 	{
-		if (!is_delimiter(str[i], charset) && index < 0)
+		if (!is_delim(str[i], charset) && index < 0)
 			index = i;
-		else if ((is_delimiter(str[i], charset) || i == strlen(str)) && index >= 0)
+		else if ((is_delim(str[i], charset) || i == strlen(str)) && index >= 0)
 		{
 			split[j++] = word_dup(str, index, i);
 			index = -1;
@@ -122,12 +116,18 @@ char	**ft_split(char *str, char *charset)
 	return (split);
 }
 
-#include <stdio.h>
-
-int main (int ac, char **av)
+int	main(void)
 {
-	char str[] = "Hola 42 How Estas You?";
-	char chr[] = " ";
-	printf("%s",ft_split(str, chr))
+	char	*str = "Hello, world! This is a test split.";
+//	char	*charset = " ,!.";
+	char	*charset = " ";
+	char	**split = ft_split(str, charset);
+	int 	i = 0;
+	
+	while (split[i])
+	{
+		printf("%s\n", split[i]);
+		i++;
+	}
 	return 0;
 }
