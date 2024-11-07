78c47b6198c8928ef255b85ef9ff3013838bbdaa
Diego <diegonmarcos@gmail.com>
2024-11-07 19:20:06 +0100

    up

--- 1.Projects/C0_Reloaded/src/ex22/ft_abs.h
+++ 1.Projects/C0_Reloaded/src/ex22/ft_abs.h
@@ -6,27 +6,13 @@
 /*   By: dinepomu <dinepomu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 13:22:48 by dinepomu          #+#    #+#             */
-/*   Updated: 2024/11/07 17:50:55 by dinepomu         ###   ########.fr       */
+/*   Updated: 2024/11/07 17:57:39 by dinepomu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_ABS_H
 # define FT_ABS_H
 
-int	ft_abs(int x)
-{
-	if (x < 0)
-		return (-x);
-	else
-		return (x);
-}
-
-#endif
-/*
-#ifndef FT_ABS_H
-# define FT_ABS_H
-
 # define ABS(x) ((x) < 0 ? -(x) : (x))
 
 #endif
-*/
