eb7876ac38ea90a6b4f42a1510d25e87538aca02
diegonmarcos <diegonmarcos@gmail.com>
2024-11-10 14:59:40 +0100

    up

--- 1.Projects/C0_libft/3.Code/libft/ft_lstmap.c
+++ 1.Projects/C0_libft/3.Code/libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: dinepomu <dinepomu@student.42>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 10:57:37 by dinepomu          #+#    #+#             */
-/*   Updated: 2024/11/10 14:57:16 by dinepomu         ###   ########.fr       */
+/*   Updated: 2024/11/10 14:59:30 by dinepomu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,49 +27,6 @@
  * @updated: 10/Nov/2024 11:53
  * ************************************************************************** *
  */
-
-#include "libft.h"
-/*
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-{
-	t_list	*new_lst;
-	t_list	*tmp;
-
-	if (!lst || !f || !del)
-		return (NULL);
-	new_lst = ft_lstnew(f(lst->content));
-	if (!new_lst)
-		return (NULL);
-	lst = lst->next;
-	while (lst)
-	{
-		tmp = ft_lstnew(f(lst->content));
-		if (!tmp)
-		{
-			ft_lstclear(&new_lst, del);
-			return (NULL);
-		}
-		ft_lstadd_back(&new_lst, tmp);
-		lst = lst->next;
-	}
-	return (new_lst);
-}
-*/
-#include "libft.h" 
-
-/*
- * Iterates the list ’lst’ and applies the function ’f’ to the 
- * content of each element. Creates a new list resulting of the 
- * successive applications of the function ’f’. The ’del’ function 
- * is used to delete the content of an element if needed.
- */
-#include "libft.h"
-
-#include "libft.h"
-
-#include "libft.h"
-
-#include "libft.h"
 
 #include "libft.h"
 
