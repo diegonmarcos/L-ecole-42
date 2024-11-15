32ded5596f8fd2bfd767ebe9a0bfb67dede34c33
Diego <diegonmarcos@gmail.com>
2024-11-15 22:54:38 +0100

    up

--- 1.Projects/C0_libft/4.Tester/warmachine/my_config.sh
+++ 1.Projects/C0_libft/4.Tester/warmachine/my_config.sh
@@ -5,7 +5,7 @@
 ###################################
 
 #Edit the next line with the path of your libft project.
-PATH_LIBFT="../0.libft"
+PATH_LIBFT="../0_libft"
 
 #If you use a relative path, don't put double quotes.
 #For example :
