83bcdc7bb81863cd7276ac9fe5a03f555f4407f3
Diego <diegonmarcos@gmail.com>
2024-11-19 15:33:51 +0100

    up

--- 1.Projects/C0_libft/3.Code/src/libft.h
+++ 1.Projects/C0_libft/3.Code/src/libft.h
@@ -6,7 +6,7 @@
 /*   By: dinepomu <dinepomu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 14:47:54 by dnepomuc          #+#    #+#             */
-/*   Updated: 2024/11/15 18:04:05 by dinepomu         ###   ########.fr       */
+/*   Updated: 2024/11/19 15:06:43 by dinepomu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,44 +17,80 @@
 # include <unistd.h>
 # include <stddef.h>
 
-// MANDATORY PART
-int		ft_atoi(const char *str);
-void	ft_bzero(void *s, size_t n);
+// ...MANDATORY PART...
+
+// --- 1.	Memory Management --- 
+
+// --- 1.1	Allocating Memory ---
+// ft_malloc
+// ft_free
+// ft_realloc
 void	*ft_calloc(size_t count, size_t size);
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-char	*ft_itoa(int n);
+
+// --- 1.2	Manipulating Memory ---
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+
+// --- 2.	String Manipulation Functions ---
+
+// --- 2.1	Comparisons and Len ---
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+size_t	ft_strlen(const char *str);
+
+// --- 2.2	Concatenation, Splitting, and Joining
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	**ft_split(const char *s, char c);
+
+// --- 2.3	Copying
+// strcpy
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_strdup(const char *s1);
+
+// --- 2.4 	Search
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+
+// 3. Character Type Checking and Conversions Functions
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_atoi(const char *str);
+char	*ft_itoa(int n);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+
+// 4. Input/Output Functions
+// --- 4.1	Standard Output
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr_fd(char *s, int fd);
-char	**ft_split(const char *s, char c);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(const char *s1);
-char	*ft_strjoin(char const *s1, char const *s2);
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlen(const char *str);
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
-char	*ft_strrchr(const char *s, int c);
-char	*ft_strtrim(char const *s1, char const *set);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
-// BONUS PART - LINKED LISTS
+// --- 4.2	File input/output
+// fopen
+// fclose
+// fread
+// fwrite
+
+// 5. String Methods
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+
+// ... BONUS PART ...
+
+// 5. Struct - Linked List Functions
 
 typedef struct s_list
 {
