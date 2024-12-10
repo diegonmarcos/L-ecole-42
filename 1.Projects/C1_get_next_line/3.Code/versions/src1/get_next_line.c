f8a9c7fbe98ea3ce45b3445016b0e48f4fcd8b66
Diego <diegonmarcos@gmail.com>
2024-12-10 12:39:35 +0100

    up

---
+++ 1.Projects/C1_get_next_line/3.Code/versions/src1/get_next_line.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dinepomu <dinepomu@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/16 10:12:14 by dnepomuc          #+#    #+#             */
+/*   Updated: 2024/11/25 17:02:24 by dinepomu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <unistd.h>
+
+char	*ft_read_to_left_str(int fd, char *left_str)
+{
+	char	*buff;
+	int		rd_bytes;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	rd_bytes = 1;
+	while (!ft_strchr(left_str, '\n') && rd_bytes != 0)
+	{
+		rd_bytes = read(fd, buff, BUFFER_SIZE);
+		if (rd_bytes == -1)
+		{
+			free(buff);
+			free(left_str);
+			return (NULL);
+		}
+		buff[rd_bytes] = '\0';
+		left_str = ft_strjoin(left_str, buff);
+	}
+	free(buff);
+	return (left_str);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*left_str;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	left_str = ft_read_to_left_str(fd, left_str);
+	if (!left_str)
+		return (NULL);
+	line = ft_get_line(left_str);
+	left_str = ft_new_left_str(left_str);
+	return (line);
+}
+/*
+#include <stdio.h>
+#include <fcntl.h>
+int	main(void)
+{
+	char	*line;
+	int		i;
+	int		fd1;
+	int		fd2;
+	int		fd3;
+	fd1 = open("../../4.Tester/test.txt", O_RDONLY);
+	fd2 = open("../../4.Tester/test1.txt", O_RDONLY);
+	fd3 = open("../../4.Tester/corrupted_file.txt", O_RDONLY);
+
+	i = 1;
+	printf("##############FRIST FILE\n");
+	while ((line = get_next_line(fd1)))
+	{
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		i++;
+	}
+	i = 0;
+	printf("\n##############SECOND FILE\n");
+	while ((line = get_next_line(fd2)))
+	{
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		i++;
+	}
+	i = 0;
+	printf("\n##############THIRD FILE\n");
+	while ((line = get_next_line(fd3)))
+	{
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		i++;
+	}
+	close(fd1);
+	close(fd2);
+	close(fd3);
+	return (0);
+}*/
---
+++ 1.Projects/C1_get_next_line/3.Code/versions/src1/get_next_line.h
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dinepomu <dinepomu@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/16 10:12:35 by dinepomu          #+#    #+#             */
+/*   Updated: 2024/12/02 15:43:59 by dinepomu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+// --- LIBFT FUNCTIONS ---
+size_t	ft_strlen(char *s);
+/**
+* @brief Returns the size of the string.
+*/
+
+char	*ft_strchr(char *s, int c);
+/**
+ * @brief Returns a pointer to the first occurrence of the character c in the
+ * 			string s.
+ */
+
+char	*ft_strjoin(char *left_str, char *buff);
+/**
+ * @brief Concatenates two strings and returns a pointer to the new string.
+ */
+
+// --- GET_NEXT_LINE FUNCTIONS ---
+char	*ft_read_to_left_str(int fd, char *left_str);
+/**
+ * @brief Updates the left_str by removing the line that has been read.
+ * @param left_str The string to update.
+ */
+
+char	*ft_new_left_str(char *left_str);
+/**
+ * @brief Extracts the next line from the left_str.
+ * @param left_str The string to extract the line from.
+ * @return A pointer to the extracted line.
+ */
+
+char	*ft_get_line(char *left_str);
+/**
+ * @brief Reads the next line from the file descriptor.
+ * @param fd The file descriptor to read from.
+ * @return A pointer to the next line read from the file descriptor.
+ */
+
+char	*get_next_line(int fd);
+/**
+ * @brief Reads the next line from the file descriptor.
+ * @param fd The file descriptor to read from.
+ * @return A pointer to the next line read from the file descriptor.
+ */
+
+char	*ft_read_to_left_str(int fd, char *left_str);
+/**
+ * @brief Reads the next line from the file descriptor.
+ * @param fd The file descriptor to read from.
+ * @param left_str The string to update.
+ * @return A pointer to the next line read from the file descriptor.
+ */
+
+char	*ft_new_left_str(char *left_str);
+/**
+ * @brief Updates the left_str by removing the line that has been read.
+ * @param left_str The string to update.
+ * @return A pointer to the new left_str.
+ */
+
+char	*ft_get_line(char *left_str);
+/**
+ * @brief Extracts the next line from the left_str.
+ * @param left_str The string to extract the line from.
+ * @return A pointer to the extracted line.
+ */
+
+char	*get_next_line(int fd);
+/**
+ * @brief Reads the next line from the file descriptor.
+ * @param fd The file descriptor to read from.
+ * @return A pointer to the next line read from the file descriptor.
+ */
+
+#endif
