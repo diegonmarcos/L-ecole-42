4ebb7ad3eefba827197aa4aaa847e479f3f9a4db
Diego <diegonmarcos@gmail.com>
2024-12-12 13:05:51 +0100

    up

---
+++ 1.Projects/C2_ExamRank02/Questions/Level 2/String_Manip_Compare/ft_strcspn/ft_strcspn.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+
+size_t	ft_strcspn(const char *s, const char *reject)
+{
+    size_t count;
+    size_t i;
+
+    count = 0;
+    i = 0;
+    while (*s)
+    {
+        while (reject[i] && *s != reject[i])
+            i++;
+        if (reject[i] != '\0')
+            return (count);
+        i = 0;
+        count++;
+        s++;
+    }
+    return (count);
+}
