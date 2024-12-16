23dd3f41cb1bf997104b08cabce5bdfa21106d27
Diego <diegonmarcos@gmail.com>
2024-12-16 20:20:07 +0100

    up

--- 1.Projects/C2_ExamRank02/type/algos/L4_algo_sorting/L4_sort_list/sort_list.c
+++ 1.Projects/C2_ExamRank02/type/algos/L4_algo_sorting/L4_sort_list/sort_list.c
@@ -9,8 +9,26 @@ struct s_list
 */
 
 #include <stdlib.h>
-#include "list.h"
+#include "sort_list.list.h"
+#include<stdio.h>
 
+// UTILS FUNC
+//#########################################################
+int ascending(int a, int b)
+{
+	return (a <= b);
+}
+
+void print_list(t_list *lst) {
+    while (lst != NULL) {
+        printf("%d -> ", lst->data);
+        lst = lst->next;
+    }
+    printf("NULL\n");
+}
+
+// MAIN FUNC
+//########################################################
 t_list *sort_list(t_list* lst, int (*cmp)(int, int)) {
     int swap;
     t_list *start;
@@ -30,4 +48,37 @@ t_list *sort_list(t_list* lst, int (*cmp)(int, int)) {
 		lst = lst->next;
     }
     return (start);
+}
+
+// MAIN
+//########################################################
+int main() {
+    // Create a sample unsorted list
+    t_list *list = malloc(sizeof(t_list));
+    list->data = 5;
+    list->next = malloc(sizeof(t_list));
+    list->next->data = 200;
+    list->next->next = malloc(sizeof(t_list));
+    list->next->next->data = 8;
+    list->next->next->next = malloc(sizeof(t_list));
+    list->next->next->next->data = 1;
+    list->next->next->next->next = NULL;
+
+    printf("Unsorted list: ");
+    print_list(list);
+
+    // Sort the list in ascending order
+    list = sort_list(list, ascending);
+
+    printf("Sorted list: ");
+    print_list(list);
+
+    // Free the allocated memory (important!)
+    while (list != NULL) {
+        t_list *temp = list;
+        list = list->next;
+        free(temp);
+    }
+
+    return 0;
 }
