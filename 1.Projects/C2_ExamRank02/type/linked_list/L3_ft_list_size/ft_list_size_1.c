3031fd0404918eb4d2da69ffd46950e8411ead01
Diego Nepomuceno Marcos <100614489+diegonmarcos@users.noreply.github.com>
2024-12-17 07:07:33 +0100

    Actualizar ft_list_size_1.c

--- 1.Projects/C2_ExamRank02/type/linked_list/L3_ft_list_size/ft_list_size_1.c
+++ 1.Projects/C2_ExamRank02/type/linked_list/L3_ft_list_size/ft_list_size_1.c
@@ -1,19 +1,48 @@
+#########################
+// # Linked Lists - Size
+// metadata
+// documentation
+
+#########################
+// # HEADERS1
+
+Def S1 0
+S1o ""
+S2c ""
+Def S2 1
+S2o "/*"
+S2c "*/"
+#########################
+// # HEADERS2
+
+#include <unistd.h>
 typedef struct s_list {
 	struct s_list next;
 	void data;
 }	t_list;
 
-// One wa
-int ft_list_size(t_list *node){
-	int i = 0;
-	while(node)
-		i++; node = node->next;
-	return(i)
+#########################
+// # SRC
+
+//   ##UTILS
+void print_bits(unsigned char octet){
 }
 
-// Second way
+//   ##MAIN 
+
+// One way S1o
+ int ft_list_size(t_list *node){
+ 	int i = 0;
+ while(node)
+	  i++; node = node->next;
+	return(i)
+}S1c
+
+// Second way S2o
 int ft_list_size(t_list *node){
 	for(int i = 0; node; node = node->next, i++);
 	return(i);
-}
+}S2c
+
+#########################
 
